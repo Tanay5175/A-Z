b8798fbe725bea3c1b534bddf5d257a9f2fc04c1
tanay5175 <tanay5175@gmail.com>
2021-06-29 19:50:18 +0530

    final commit

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")
-('sk_test_51J2iykSApei5HcoUuwKhv783UcZZkorYLOiJnDalHpoZ1gCSUBYxqsNJdNKL7Vke2uAZo16WWAyNaunUF94XIhy4008ELGDB41');
+();
 
 //API
 //App config
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAIQ-HVnK5MQCwI_2R04DgHASQLsPI58Uw",
-    authDomain: "e-shop-4a7b7.firebaseapp.com",
-    projectId: "e-shop-4a7b7",
-    storageBucket: "e-shop-4a7b7.appspot.com",
-    messagingSenderId: "400873702392",
-    appId: "1:400873702392:web:94a0c36a37e7c053d64c0d"
+    
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
